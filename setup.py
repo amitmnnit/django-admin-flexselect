781a475b2ab4b23139822f283a77c60dabf8f14e
Nishad Musthafa <nishadmusthafa@gmail.com>
2012-02-16 19:38:34 +0530

    First commit

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
     name='django-admin-flexselect',
     version='0.4.0',
-    author=u'Rune Kaagaard',
-    author_email='rumi.kg@gmail.com',
+    author=u'Nishad Musthafa',
+    author_email='nishadmusthafa@gmail.com',
     packages=['flexselect'],
-    url='https://github.com/runekaagaard/django-admin-flexselect',
+    url='https://github.com/plivo/django-admin-flexselect',
     license='Public Domain',
-    description='Dynamic select fields for the Django Admin that just works.',
+    description='Dynamic select fields for the Django Admin that just works combined with chosen.js.',
     long_description=open('README.markdown').read(),
     package_data={'flexselect': ['static/flexselect/js/flexselect.js']},
 )
